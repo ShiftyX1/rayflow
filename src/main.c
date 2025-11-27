f29271704ed9ebbbf8d2a14dd8f942d733bfd1cd
Илья Глазунов <i.glazunov@sapiens.solutions>
2025-11-27 05:17:29 +0300

    fonts and Cyrillic added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "nuklear_raylib.h"
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_TEXT_LENGTH 256
 
@@ -11,15 +12,32 @@ int main() {
     InitWindow(screenWidth, screenHeight, "PULSE engine TEST");
     SetTargetFPS(60);
     
+    // Change working directory to project root
+    ChangeDirectory("/Users/shifty/projects/raylib-test");
+    
+    // Load range of characters including Cyrillic
+    // 0x0020-0x007F - basic Latin
+    // 0x0400-0x04FF - Cyrillic
+    int codepointCount = 0;
+    int *codepoints = LoadCodepoints("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~", &codepointCount);
+    
+    Font font = LoadFontEx("src/static/fonts/Inter_18pt-Regular.ttf", 20, codepoints, codepointCount);
+    UnloadCodepoints(codepoints);
+    
+    if (font.texture.id == 0) {
+        font = GetFontDefault();
+    }
+    
     nk_raylib_init();
     
-    static char text_buffer[MAX_TEXT_LENGTH] = "Hello from raylib!";
+    static char text_buffer[MAX_TEXT_LENGTH] = "Привет! Hello!";
     int text_len = strlen(text_buffer);
+    static struct nk_colorf text_color = {0.2f, 0.2f, 0.2f, 1.0f};
     
     while (!WindowShouldClose()) {
         nk_raylib_handle_input();
         
-        if (nk_begin(&nk_raylib.ctx, "Text Editor", nk_rect(50, 50, 400, 150),
+        if (nk_begin(&nk_raylib.ctx, "Text Editor", nk_rect(50, 50, 400, 250),
             NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
             NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE)) {
             
@@ -29,13 +47,25 @@ int main() {
             nk_layout_row_dynamic(&nk_raylib.ctx, 30, 1);
             nk_edit_string(&nk_raylib.ctx, NK_EDIT_FIELD, text_buffer, &text_len, MAX_TEXT_LENGTH, nk_filter_default);
             text_buffer[text_len] = '\0';
+            
+            nk_layout_row_dynamic(&nk_raylib.ctx, 30, 1);
+            nk_label(&nk_raylib.ctx, "Text Color:", NK_TEXT_LEFT);
+            
+            nk_layout_row_dynamic(&nk_raylib.ctx, 120, 1);
+            text_color = nk_color_picker(&nk_raylib.ctx, text_color, NK_RGBA);
         }
         nk_end(&nk_raylib.ctx);
         
         BeginDrawing();
         ClearBackground(RAYWHITE);
         
-        DrawText(text_buffer, 190, 300, 20, DARKGRAY);
+        Color raylib_color = {
+            (unsigned char)(text_color.r * 255),
+            (unsigned char)(text_color.g * 255),
+            (unsigned char)(text_color.b * 255),
+            (unsigned char)(text_color.a * 255)
+        };
+        DrawTextEx(font, text_buffer, (Vector2){190, 300}, 20, 1, raylib_color);
         DrawFPS(10, 10);
         
         nk_raylib_render();
@@ -44,6 +74,7 @@ int main() {
     }
     
     nk_raylib_shutdown();
+    UnloadFont(font);
     CloseWindow();
     
     return 0;
